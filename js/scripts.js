1c6180d7baccb134b36aab5aaa024aa2200bdc65
Ben Logue <benjamin.logue73@gmail.com>
2016-06-18 16:36:57 -0700

    it will add object constructor and new methods for object player.

--- js/scripts.js
+++ js/scripts.js
@@ -1,57 +1,85 @@
-var diceRoll = function(){
-  return Math.floor(Math.random() * 6 + 1);
+var playerOneTurn = true;
+
+function Player(name, playertype) {
+  this.name = name;
+  this.player = playertype;
+  this.points = 0;
+  this.roundPoints = 0;
+}
+
+Player.prototype.diceRoll = function(){
+   var currentRoll = Math.floor(Math.random() * 6 + 1);
+   this.roundPoints += currentRoll;
+   return currentRoll;
+}
+
+Player.prototype.hold = function(){
+  this.points += this.roundPoints;
 }
 
 
 $(document).ready(function(){
-    var playerOne = {
-      pointTotal: 0,
-      turn: 2
-    }
+  var playerOne;
+  var playerTwo;
+  var dice = ["&#9856", "&#9857", "&#9858", "&#9859", "&#9860", "&#9861"];
 
-    var playerTwo = {
-      pointTotal: 0,
-      turn: 1
-    }
-    var roundPoints = 0;
+  var diceDisplay = function(side){
+     return $("#dice-roll").html("<h1>" + dice[side - 1] + "</h1>");
+
+  }
+
+  $("#start-game").click(function(){
+     playerOne = new Player("John", "human");
+     playerTwo = new Player("Hal", "computer")
+  });
 
       $("#roll").click(function(){
-        roll = diceRoll();
-        roundPoints += roll;
-        if (playerOne.turn % 2 === 0 && roll === 1) {
-          $("#player-turn").text("Player Two Turn");
-          playerOne.turn += 1;
-          roundPoints = 0;
-          $("#round-points").text(roundPoints);
-        } else if (playerOne.turn % 2 !== 0 && roll === 1) {
+
+        if (playerOneTurn === true) {
           $("#player-turn").text("Player One Turn");
-          playerOne.turn += 1;
-          roundPoints = 0;
-          $("#round-points").text(roundPoints);
+           currentRoll = playerOne.diceRoll();
+            if (currentRoll === 1) {
+              alert(playerOne.name + " rolled a 1 and loses their turn.");
+              playerOne.roundPoints = 0;
+              playerOneTurn = false;
+              $("#player-turn").text("Player Two Turn");
+            }
+
+          diceDisplay(currentRoll);
+          $("#round-points").text(playerOne.roundPoints);
+
+            } else  {
+            $("#player-turn").text("Player Two Turn");
+             currentRoll = playerTwo.diceRoll();
+              if (currentRoll === 1) {
+                alert(playerTwo.name + " rolled a 1 and loses their turn.");
+                playerTwo.roundPoints = 0;
+                playerOneTurn = true;
+                $("#player-turn").text("Player One Turn");
+              }
+
+            diceDisplay(currentRoll);  
+            $("#round-points").text(playerTwo.roundPoints);
         }
-
-        $("#dice-roll").text(roll);
-        $("#round-points").text(roundPoints);
-
       });
 
       $("#pig-dice-game").submit(function(event){
         event.preventDefault();
-        if (playerOne.turn % 2 === 0) {
+        if (playerOneTurn === true) {
         $("#player-turn").text("Player Two Turn");
 
-        playerOne.pointTotal += roundPoints;
-        $('#player-one-points').text(playerOne.pointTotal);
-        playerOne.turn += 1;
-        roundPoints = 0;
-        $("#round-points").text(roundPoints);
+        playerOne.points += playerOne.roundPoints;
+        $('#player-one-points').text(playerOne.points);
+        playerOneTurn = false;
+        playerOne.roundPoints = 0;
+        $("#round-points").text(playerOne.roundPoints);
       } else {
         $("#player-turn").text("Player One Turn");
-        playerTwo.pointTotal += roundPoints;
-        $('#player-two-points').text(playerTwo.pointTotal);
-        playerOne.turn += 1;
-        roundPoints = 0;
-        $("#round-points").text(roundPoints);
+        playerTwo.points += playerTwo.roundPoints;
+        $('#player-two-points').text(playerTwo.points);
+        playerOneTurn = true;
+        playerTwo.roundPoints = 0;
+        $("#round-points").text(playerOne.roundPoints);
       }
   });
 });
