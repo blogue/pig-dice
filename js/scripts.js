6cbc3a74e4e399c87be7115be2eafad704fdd54d
Ben Logue <benjamin.logue73@gmail.com>
2016-06-24 19:35:24 -0700

    add game object and game functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,90 +1,108 @@
-var playerOneTurn = true;
-
-function Player(name, playertype) {
+function Player(name, playerType) {
   this.name = name;
-  this.player = playertype;
+  this.playerType = playerType;
   this.points = 0;
   this.roundPoints = 0;
-}
+};
 
-Player.prototype.diceRoll = function(){
-   var currentRoll = Math.floor(Math.random() * 6 + 1);
-   this.roundPoints += currentRoll;
-   return currentRoll;
-}
+function Game() {
+  this.players = [];
+  this.turn = 0;
+  this.winner = false;
+};
 
-Player.prototype.hold = function(){
-  this.points += this.roundPoints;
-}
+Game.prototype.whoseTurn = function(){
+  return this.players[this.turn%this.players.length];
+};
 
-$(document).ready(function(){
-  var playerOne;
-  var playerTwo;
-  var dice = ["&#9856", "&#9857", "&#9858", "&#9859", "&#9860", "&#9861"];
-
-  var diceDisplay = function(side){
-     return $("#dice-roll").html("<h1>" + dice[side - 1] + "</h1>");
-
+Game.prototype.displayPlayerInfo = function(){
+  var currentPlayer = this.whoseTurn();
+  $("#round-points").text(currentPlayer.roundPoints);
+  $("#player-turn").text(currentPlayer.name + "'s Turn.");
+  $("#playerOnePoints").text(this.players[0].name + "'s points: " + this.players[0].points);
+  $("#playerTwoPoints").text(this.players[1].name + "'s points: " + this.players[1].points);
+  if (this.whoseTurn() === this.players[0]) {
+    $("#playerOneCard").removeClass("inactive-player").addClass("active-player");
+  } else {
+    $("#playerOneCard").removeClass("active-player").addClass("inactive-player");
   }
+  if (this.whoseTurn() === this.players[1]) {
+    $("#playerTwoCard").removeClass("inactive-player").addClass("active-player");
+  } else {
+    $("#playerTwoCard").removeClass("active-player").addClass("inactive-player");
+  }
+};
 
-  $("#start-game").click(function(){
-     playerOne = new Player("John", "human");
-     playerTwo = new Player("Hal", "computer")
+Game.prototype.rollDice = function() {
+  var currentPlayer = this.whoseTurn();
+  var currentRoll = Math.ceil(Math.random() * 6);
+  if (currentRoll === 1) {
+   currentPlayer.roundPoints = 0;
+   this.turn++;
+   this.displayPlayerInfo();
+   return currentRoll;
+ } else {
+  currentPlayer.roundPoints += currentRoll;
+  this.displayPlayerInfo();
+  return currentRoll;
+  }
+};
+
+Game.prototype.roundPointsToTotal = function() {
+  debugger;
+  var currentPlayer = this.whoseTurn();
+  currentPlayer.points += currentPlayer.roundPoints;
+  currentPlayer.roundPoints = 0;
+  if (currentPlayer.points >= 30) {
+    this.displayPlayerInfo();
+    return this.winner = true;
+  } else {
+  this.turn++;
+  this.displayPlayerInfo();
+  }
+};
+
+$(document).ready(function() {
+  var game = new Game();
+  game.players.push(new Player("John", "human"));
+  game.players.push(new Player("Hal", "computer"));
+  var dice = ["&#9856", "&#9857", "&#9858", "&#9859", "&#9860", "&#9861"];
+  var diceDisplay = function(side) {
+     return $("#dice-roll").html("<h1>" + dice[side - 1] + "</h1>");
+  }
+  $("#player-turn").text(game.players[0].name + "'s Turn");
+  $("#round-points").text(0);
+  $("#playerOnePoints").text(game.players[0].name + "'s points: " + 0);
+  $("#playerTwoPoints").text(game.players[1].name + "'s points: " + 0);
+  $("#dice-roll").html("<h1>" + dice[5] + "</h1>");
+
+  $("#roll").click(function(){
+
+    if (game.whoseTurn().playerType === "human") {
+      diceDisplay(game.rollDice());
+      game.displayPlayerInfo();
+    } else {
+    debugger;
+    while (game.whoseTurn().playerType === "computer") {
+      diceDisplay(game.rollDice());
+        if (game.whoseTurn().roundPoints >= 12) {
+          game.roundPointsToTotal();
+          if (game.winner) {
+            $("#player-turn").text(game.whoseTurn().name.toUpperCase() + "'S THE WINNER!");
+            $("#hold, #roll").attr("disabled", true);
+            break;
+          }
+        }
+      }
+    }
   });
 
-      $("#roll").click(function(){
-
-        if (playerOneTurn === true) {
-          $("#player-turn").text("Player One Turn");
-           currentRoll = playerOne.diceRoll();
-            if (currentRoll === 1) {
-              alert(playerOne.name + " rolled a 1 and loses their turn.");
-              playerOne.roundPoints = 0;
-              playerOneTurn = false;
-              $("#player-turn").text("Player Two Turn");
-            }
-
-          diceDisplay(currentRoll);
-          $("#round-points").text(playerOne.roundPoints);
-
-            } else  {
-            $("#player-turn").text("Player Two Turn");
-             currentRoll = playerTwo.diceRoll();
-              if (currentRoll === 1) {
-                alert(playerTwo.name + " rolled a 1 and loses their turn.");
-                playerTwo.roundPoints = 0;
-                playerOneTurn = true;
-                $("#player-turn").text("Player One Turn");
-              }
-
-            diceDisplay(currentRoll);
-            $("#round-points").text(playerTwo.roundPoints);
-        }
-      });
-
-      $("#pig-dice-game").submit(function(event){
-        event.preventDefault();
-        if (playerOneTurn === true) {
-        $("#player-turn").text("Player Two Turn");
-
-        playerOne.points += playerOne.roundPoints;
-        $('#player-one-points').text(playerOne.points);
-        if (playerOne.points >= 30) {
-          alert(playerOne.name + " wins!");
-        }
-        playerOneTurn = false;
-        playerOne.roundPoints = 0;
-        $("#round-points").text(playerOne.roundPoints);
-      } else {
-        $("#player-turn").text("Player One Turn");
-        playerTwo.points += playerTwo.roundPoints;
-        $('#player-two-points').text(playerTwo.points);
-        if (playerTwo.points >= 30) {
-          alert(playerTwo.name + " wins!");
-        }
-        playerOneTurn = true;
-        playerTwo.roundPoints = 0;
-        $("#round-points").text(playerOne.roundPoints);
-      }
+  $("#hold").click(function(){
+    debugger;
+    game.roundPointsToTotal();
+    if(game.winner){
+      $("#player-turn").text(game.whoseTurn().name.toUpperCase() + "'S THE WINNER!");
+      $("#hold, #roll").attr("disabled", true);
+    }
   });
 });
